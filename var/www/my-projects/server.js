9b53b80b2f8909ad264b60f15b01a3aea45e9c8b
auraecosystem <sandraisom@icloud.com>
2026-05-30 22:50:37 +0100

    Update server.js
    Signed-off-by: auraecosystem <sandraisom@icloud.com>

--- var/www/my-projects/server.js
+++ var/www/my-projects/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const UPLOAD_DIR = process.env.UPLOAD_DIR;
 
 app.use(cors());
