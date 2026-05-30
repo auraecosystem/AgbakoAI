4864e15e5a08cf84f4b1b74e7a2880927daa0fd1
auraecosystem <sandraisom@icloud.com>
2026-05-30 23:09:00 +0100

    Update and rename Brew.sh to Brew.bash
    Signed-off-by: auraecosystem <sandraisom@icloud.com>

--- Brew.bash
+++ Brew.bash
@@ -1,6 +1,7 @@
 ./config.sh --url https://github.com/QUBUHUB-incs/AgbakoAI --token BH6MBWC5MQDJJEF7AG474WTIEKNEM
 curl -o actions-runner-linux-arm64-2.323.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-arm64-2.323.0.tar.gz
-
+sudo mkdir -p /var/www/sites/myproject
+echo "<h1>My Project</h1>" | sudo tee /var/www/sites/myproject/index.html
 echo "9cb778fffd4c6d8bd74bc4110df7cb8c0122eb62fda30b389318b265d3ade538  actions-runner-linux-arm64-2.323.0.tar.gz" | shasum -a 256 -c
 
 tar xzf ./actions-runner-linux-arm64-2.323.0.tar.gz
@@ -8,8 +9,8 @@ tar xzf ./actions-runner-linux-arm64-2.323.0.tar.gz
 pip install speechrecognition transformers pyttsx3 torch
 pip install fastapi uvicorn torch
 pip install fastapi uvicorn torch
-uvicorn neo_server_real:app --reload --host 0.0.0.0 --port 8000
-uvicorn neo_server_real:app --reload --host 0.0.0.0 --port 8000
+uvicorn neo_server_real:app --reload --host 10.0.0.1 --port 3000
+uvicorn neo_server_real:app --reload --host 127.0.0.0 --port 8000
 # For Python dependencies
 pip install -r requirements.txt
 
@@ -19,4 +20,4 @@ npm install
 git status
 git add agbakoAI.py .gitignore requirements.txt  # add more files if needed
 git commit -m "Initial commit of AgbakoAI project files"
-git push origin master
+git push origin main
